54ac0238f46d432490851689e97f1bae468d94e2
bekirberksenel <bekirberksenel2701@gmail.com>
2017-09-23 20:24:52 +0300

    Last Commit

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -26,7 +26,7 @@ public class AppTest
     /**
      * @return the suite of tests being tested
      */
-    public static void test2()
+    public void test2()
     {
 	App pp = new App();
 	String[] arr1 = {"Bekir","Berk","Omer","Fatih","Furkan","Can","Mehmet"};
@@ -37,21 +37,21 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public static void test3()
+    public void test3()
     {
 	App ppp = new App();
 	String[] arr1 = {"Bekir","Bekir","Omer","Fatih","Furkan","Can","Mehmet"};
  	assertTrue(((ppp.iceriyorMu(arr1,27,"Bekir")).equals("Array Bekir iceriyor."))); 
         
     }
-    public static void test4()
+    public void test4()
     {
 	App pppp = new App();
 	String[] arr1 = {"Bekir","Berk","Omer","Fatih","Furkan","Can","Mehmet"};
  	assertTrue(((pppp.iceriyorMu(arr1,27,"Berk")).equals("Array Berk iceriyor."))); 
         
     }
-    public static void test5()
+    public void test5()
     {
 	App ppppp = new App();
 	String[] arr1 = {""};
