32f0772d850098d00ac2d35dbae499c5cc3b35cb
bekirberksenel <bekirberksenel2701@gmail.com>
2017-09-24 16:03:15 +0300

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,5 @@
 package com.mycompany.app;
+
 import static spark.Spark.*;
 
 import java.util.HashMap;
@@ -8,7 +9,6 @@ import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
-
 /**
  * Hello world!
  *
@@ -17,7 +17,40 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+       	  String input1 = "Turkey";
+          String[] inputList = new String[7];
+  
+  	  for(int i = 0 ; i<inputList.length;i++){
+	 	System.out.println(inputList[i]);
+	  }
+
+
+          int input2 = 7;
+
+          String result = App.iceriyorMu(inputList, input2,input1);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+        (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+
     }
     public static String iceriyorMu(String[] arr,int number,String str )
     { 
@@ -36,4 +69,12 @@ public class App
 	return "Array " + str + " iceriyor.";
    	
     }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
