11aeb73b9994fb8e9123407407e504c8ff2915e5
bekirberksenel <bekirberksenel2701@gmail.com>
2017-09-24 17:04:50 +0300

    Last commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,26 +1,20 @@
 package com.mycompany.app;
-
 import static spark.Spark.*;
-
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 /**
  * Hello world!
  *
  */
-public class App
-{
-    public static boolean find(ArrayList<String> arr, int input, String str) {
+public class App{
+    public static boolean search(ArrayList<String> arr, int input, String str){
       int count = 0;
-      System.out.println("");
-	 
+      System.out.println("Searching..");
       if (array == null) return false;
-
     	for(int i = 0; i<arr.size(); i++){
 		if(arr.get(i).equals(str)){
 			count++;	
@@ -31,16 +25,12 @@ public class App
       }
       return false;
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         port(getHerokuAssignedPort());
-
         get("/", (req, res) -> "Hello, World");
-
         post("/compute", (req, res) -> {
           //System.out.println(req.queryParams("input1"));
           //System.out.println(req.queryParams("input2"));
-
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
@@ -51,21 +41,14 @@ public class App
             inputList.add(value);
           }
           System.out.println(inputList);
-
-
           String input2 = req.queryParams("input2").replaceAll("\\s","");
-
 	  String input3 = req.queryParams("input3").replaceAll("\\s","");
 	  int input3Int = Integer.parseInt(input3);
-
-          boolean result = App.search(inputList, input2 , input3Int);
-
-         Map map = new HashMap();
+          boolean result = App.search(inputList,input3Int,input2);
+          Map map = new HashMap();
           map.put("result", result);
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
-
-
         get("/compute",
             (rq, rs) -> {
               Map map = new HashMap();
@@ -74,7 +57,6 @@ public class App
             },
             new MustacheTemplateEngine());
     }
-
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
